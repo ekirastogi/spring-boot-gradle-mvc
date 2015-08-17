cf1ffaab6eeb9dbe89f65d05b18c939cbd38b143
Ekansh <ekansh.rastogi@intelligrape.com>
2015-08-18 02:41:26 +0530

    removing unused files

--- src/main/java/com/ekiras/config/AppConfiguration.java
+++ src/main/java/com/ekiras/config/AppConfiguration.java
@@ -2,10 +2,7 @@ package com.ekiras.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
